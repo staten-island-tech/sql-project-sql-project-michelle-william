16a43d7904ebdfccaacc3fcb09d7e10e1a652b74
WilliamLi22 <forvalious@gmail.com>
2023-05-31 10:15:55 -0400

    Change of plans, signin hard

--- sql/src/supabase.js
+++ sql/src/supabase.js
@@ -1,8 +1,7 @@
-VITE_SUPABASE_URL = 'https://qtbwblsfjwdtewafoqph.supabase.co'
-VITE_SUPABASE_ANON_KEY =
-  eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9
+let VITE_SUPABASE_URL = 'https://qtbwblsfjwdtewafoqph.supabase.co'
+let VITE_SUPABASE_ANON_KEY = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9
     .eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF0YndibHNmandkdGV3YWZvcXBoIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODM4OTk1OTAsImV4cCI6MTk5OTQ3NTU5MH0
-    .aHBemK6VllFXBqZII4cBXwnYYvVJkasV4pQ - oElV0zQ
+    .aHBemK6VllFXBqZII4cBXwnYYvVJkasV4pQ - oElV0zQ`
 
 import { createClient } from '@supabase/supabase-js'
 
