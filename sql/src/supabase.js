03442425334a406e6d3b91540205eb28367492d7
michelles11 <113190099+michelles11@users.noreply.github.com>
2023-06-05 10:16:12 -0400

    idk what to do

--- sql/src/supabase.js
+++ sql/src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
-
 const supabaseUrl = 'https://qtbwblsfjwdtewafoqph.supabase.co'
 const supabaseAnonKey = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF0YndibHNmandkdGV3YWZvcXBoIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODM4OTk1OTAsImV4cCI6MTk5OTQ3NTU5MH0.aHBemK6VllFXBqZII4cBXwnYYvVJkasV4pQ-oElV0zQ`
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+
