0b06a40a09c08c5da0cea3a4f0393212e3d0df5b
WilliamLi22 <forvalious@gmail.com>
2023-06-14 00:54:54 -0400

    Caps?

--- sql/src/router/index.js
+++ sql/src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import PlayersView from '../Views/playersView.vue'
+import PlayersView from '../views/PlayersView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
