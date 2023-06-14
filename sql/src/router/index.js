cf128e7834da529cdcaec99da60b4044c947a1d1
WilliamLi22 <forvalious@gmail.com>
2023-06-13 21:42:26 -0400

    More clean up

--- sql/src/router/index.js
+++ sql/src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import SigninView from '../Views/SigninView.vue'
+import PlayersView from '../Views/PlayersView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -11,9 +11,9 @@ const router = createRouter({
       component: HomeView
     },
     {
-      path: '/Signin',
-      name: 'Signin',
-      component: SigninView
+      path: '/Players',
+      name: 'Players',
+      component: PlayersView
     }
   ]
 })
